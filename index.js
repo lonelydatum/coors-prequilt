314b821c45b013d1e9cfbf3b370d7126bfe9f527
Gar <gar@lonelydatum.com>
2022-11-23 13:21:26 -0500

    ON

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require("express");
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
-// Initialize Express
-const app = express();
-
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-
-// Initialize server
-app.listen(3000, () => {
-  console.log("Running on port 5000.");
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
