02fcaf83693813c825a3be193d709892a989f5f1
Gar <gar@lonelydatum.com>
2022-11-23 13:05:21 -0500

    on

--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import { createServer } from "cors-anywhere";
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
 
-createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeader: ["origin", "x-requested-with"],
-  removeHeaders: ["cookie", "cookie2"],
+createServer({  
+  requireHeader: ['origin', 'x-requested-with'],  
+  removeHeaders: [
+    'cookie',
+    'cookie2',
+    // Strip Heroku-specific headers
+    'x-request-start',
+    'x-request-id',
+    'via',
+    'connect-time',
+    'total-route-time',
+    // Other Heroku added debug headers
+    // 'x-forwarded-for',
+    // 'x-forwarded-proto',
+    // 'x-forwarded-port',
+  ],
+  redirectSameOrigin: true,
+  httpProxyOptions: {
+    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+    xfwd: false,
+  },
 }).listen(port, host, function () {
   console.log("Running CORS Anywhere on " + host + ":" + port);
 });
