e1813d4d03eebc67d98038f1e050514c66593552
Gar <gar@lonelydatum.com>
2022-11-23 14:16:50 -0500

    ON

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ var host = process.env.HOST || '0.0.0.0';
 var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    
-    
+cors_proxy.createServer({  
+  requireHeader: ['origin', 'x-requested-with'],  
+  removeHeaders: [
+    'cookie',
+    'cookie2',    
+    'x-request-start',
+    'x-request-id',
+    'via',
+    'connect-time',
+    'total-route-time'    
+  ],
+  redirectSameOrigin: true  
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
