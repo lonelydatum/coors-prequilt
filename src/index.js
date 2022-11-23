4725920d986e374e6b892077a79a4a6044072baf
Gar <gar@lonelydatum.com>
2022-11-23 11:52:08 -0500

    express 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-// import { createServer } from "cors-anywhere";
+import { createServer } from "cors-anywhere";
 
-// const host = process.env.HOST || "0.0.0.0";
-// const port = process.env.PORT || 8080;
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 8080;
 
-// createServer({
-//   originWhitelist: [], // Allow all origins
-//   requireHeader: ["origin", "x-requested-with"],
-//   removeHeaders: ["cookie", "cookie2"],
-// }).listen(port, host, function () {
-//   console.log("Running CORS Anywhere on " + host + ":" + port);
-// });
-
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({ name: "John Doe" });
-}
+createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ["origin", "x-requested-with"],
+  removeHeaders: ["cookie", "cookie2"],
+}).listen(port, host, function () {
+  console.log("Running CORS Anywhere on " + host + ":" + port);
+});
